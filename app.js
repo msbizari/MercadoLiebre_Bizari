4ed835e1410c3dbaddda493c6ebdc09e9726e1e9
msbizari <msbizari@gmail.com>
2021-05-25 18:16:47 -0300

    Subiendo proyecto Mercado Liebre_Bizari

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require ('express');
+const path = require ('path');
+const app = express ();
+
+/*path1 = path.resolve("user/admin", "readme.md");
+console.log(path1)*/
+
+
+//Para qué era el express.static? para que las imagenes se vean
+
+app.use(express.static('./public'));
+
+// para probar si estoy invocando bien la ruta, antes de hacer el app.get puedo probar la ruta con un console.log:
+//console.log (path.resolve ('./views/home.html'));
+app.get ('/', (req,res) => {
+    res.sendFile(path.resolve('./views/home.html'));
+    });
+
+app.get ('/home', (req,res) => {
+    res.sendFile(path.resolve('./views/home.html'));
+});
+
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
+
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
+
+app.listen(3500, () => console.log('servidor funcionando'));        
